82a25cf993b7c750e9f823f7d13a74bce33242a2
Oliver McBeth <s3mcol00@students.osao.fi>
2024-11-21 13:34:06 +0200

    index.php lisätty

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li><a href="login-form.php">Kirjaudu</a></li>
+        <li><a href="register-form.php">Rekisteröi</a></li>
+    </ul>
+</body>
+</html>
