f62d5e95ab94efbf1a2259f0eae29cc5ea81ad48
Oliver McBeth <s3mcol00@students.osao.fi>
2024-11-21 14:06:03 +0200

    change

---
+++ register-exec.php
@@ -0,0 +1,105 @@
+<?php
+	//Start session
+	session_start();
+	
+	//Include database connection details
+	require_once('config.php');
+	
+	//Array to store validation errors
+	$errmsg_arr = array();
+	
+	//Validation error flag
+	$errflag = false;
+	
+	//Connect to mysql server
+	$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
+	if(!$link) {
+		die('Failed to connect to server: ' . mysql_error());
+	}
+	
+	//Select database
+	$db = mysqli_select_db($link, DB_DATABASE);
+	if(!$db) {
+		die("Unable to select database");
+	}
+	
+	//Function to sanitize values received from the form. Prevents SQL injection
+	function clean($link, $str) {
+		$str = @trim($str);
+		return mysqli_real_escape_string($link, $str);
+	}
+	
+	//Sanitize the POST values
+	$fname = clean($link, $_POST['fname']);
+	$lname = clean($link, $_POST['lname']);
+	$email = clean($link, $_POST['email']);
+	$login = clean($link, $_POST['login']);
+	$password = clean($link, $_POST['password']);
+	$cpassword = clean($link, $_POST['cpassword']);
+	
+	//Input Validations
+	if($fname == '') {
+		$errmsg_arr[] = 'First name missing';
+		$errflag = true;
+	}
+	if($lname == '') {
+		$errmsg_arr[] = 'Last name missing';
+		$errflag = true;
+	}
+	if($email == '') {
+		$errmsg_arr[] = 'Email missing';
+		$errflag = true;
+	}
+	if($login == '') {
+		$errmsg_arr[] = 'Login ID missing';
+		$errflag = true;
+	}
+	if($password == '') {
+		$errmsg_arr[] = 'Password missing';
+		$errflag = true;
+	}
+	if($cpassword == '') {
+		$errmsg_arr[] = 'Confirm password missing';
+		$errflag = true;
+	}
+	if( strcmp($password, $cpassword) != 0 ) {
+		$errmsg_arr[] = 'Passwords do not match';
+		$errflag = true;
+	}
+	
+	//Check for duplicate login ID
+	if($login != '') {
+		$qry = "SELECT * FROM members WHERE login='$login'";
+		$result = mysqli_query($link, $qry);
+		if($result) {
+			if(mysqli_num_rows($result) > 0) {
+				$errmsg_arr[] = 'Login ID already in use';
+				$errflag = true;
+			}
+			@mysqli_free_result($result);
+		}
+		else {
+			die("Query failed");
+		}
+	}
+	
+	//If there are input validations, redirect back to the registration form
+	if($errflag) {
+		$_SESSION['ERRMSG_ARR'] = $errmsg_arr;
+		session_write_close();
+		header("location: register-form.php");
+		exit();
+	}
+
+	//Create INSERT query
+	$qry = "INSERT INTO members(firstname, lastname, email, login, passwd) VALUES('$fname','$lname','$email','$login','".md5($_POST['password'])."')";
+	$result = @mysqli_query($link, $qry);
+	
+	//Check whether the query was successful or not
+	if($result) {
+		header("location: register-success.php");
+		exit();
+	}else {
+		die("Query failed");
+	}
+?>
