3b02f8ba56a2d8a8ac20a8c6a3edc12fcd0f90b3
Oliver McBeth <s3mcol00@students.osao.fi>
2024-11-28 13:43:44 +0200

    etusivu alkuun

--- etusivu.php
+++ etusivu.php
@@ -1,1 +1,7 @@
-<p>ETUSIVU</p>
+<div style="text-align: center; color: white;">
+    <h1>KG Keittiökalusteet</h1>
+    <h3>Innovatiiviset Keittiövälineet</h3>
+    <p>Tuomme keittiöösi käytännöllisyyttä ja tyyliä! KG Keittiökalusteet tarjoaa laadukkaita keittiögadgeteja, 
+    jotka tekevät arjesta sujuvampaa ja ruoanlaitosta nautinnollisempaa.</p>
+    <a style="margin-right: 10px;" class="edit-btn" href="index.php?page=login-form">Kirjaudu sisään</a> <a class="edit-btn" href="index.php?page=register-form">Rekisteröidy</a>
+</div>
