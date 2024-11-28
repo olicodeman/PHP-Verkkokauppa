2109bc9c950eeaeb9b9bf7b34fbf07089d0bd17e
Oliver McBeth <s3mcol00@students.osao.fi>
2024-11-28 13:20:07 +0200

    error sivun muokkaus

--- error.php
+++ error.php
@@ -1,1 +1,4 @@
-<h1>Tätä sivua ei ole saatavilla.</h1>
+<div style="text-align: center;">
+    <h1>Tätä sivua ei ole saatavilla.</h1>
+    <h4><a href="index.php?page=login-form">Kirjaudu sisään</a></h4>
+</div>
