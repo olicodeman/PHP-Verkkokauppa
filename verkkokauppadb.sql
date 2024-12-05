eae5bd6c27e3a3392b07d94f352f5f8e3e026239
Oliver McBeth <s3mcol00@students.osao.fi>
2024-12-05 11:06:58 +0200

    tietokanta

--- verkkokauppadb.sql
+++ verkkokauppadb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 05.12.2024 klo 09:13
+-- Generation Time: 05.12.2024 klo 10:04
 -- Palvelimen versio: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
