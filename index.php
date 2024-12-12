ce66048b65dafd52e61e51dda6814ca38c331b2e
Oliver McBeth <s3mcol00@students.osao.fi>
2024-12-12 11:35:15 +0200

    Navbar tyyli muutokset

--- index.php
+++ index.php
@@ -22,19 +22,20 @@
 <nav class="navbar">
     <div class="menu-icon" onclick="toggleMenu()">☰</div>
     <ul class="nav-links">
-        <li><a href="index.php?page=etusivu">Etusivu</a></li>
-        <li><a href="index.php?page=tuoteet">Tuotteet</a></li>
+        <!-- Add active class if the current page matches the link -->
+        <li><a href="index.php?page=etusivu" class="<?= ($page == 'etusivu') ? 'active' : '' ?>">Etusivu</a></li>
+        <li><a href="index.php?page=tuoteet" class="<?= ($page == 'tuoteet') ? 'active' : '' ?>">Tuotteet</a></li>
+
         <?php if ($IsLoggedIn): ?>
-            <li><a href="index.php?page=profiili">Profiili</a></li>
-            <li><a href="index.php?page=logout">Kirjaudu ulos</a></li>  
+            <li><a href="index.php?page=profiili" class="<?= ($page == 'profiili') ? 'active' : '' ?>">Profiili</a></li>
+            <li><a href="index.php?page=logout" class="<?= ($page == 'logout') ? 'active' : '' ?>">Kirjaudu ulos</a></li>  
         <?php else: ?>
-            <li><a href="index.php?page=login-form">Kirjaudu sisään</a></li>
-            <li><a href="index.php?page=register-form">Rekisteröidy</a></li>
+            <li><a href="index.php?page=login-form" class="<?= ($page == 'login-form') ? 'active' : '' ?>">Kirjaudu sisään</a></li>
+            <li><a href="index.php?page=register-form" class="<?= ($page == 'register-form') ? 'active' : '' ?>">Rekisteröidy</a></li>
         <?php endif; ?>
-        <!-- ostoskori -->
-<li><a href="index.php?page=ostoskori"><div class="icon">
-    <img src="https://cdn-icons-png.flaticon.com/512/6713/6713719.png" alt="Ostoskori" class="cart-icon"></div></a></li>
     </ul>
+    <a href="index.php?page=ostoskori"><div class="icon">
+    <img src="https://cdn-icons-png.flaticon.com/512/6713/6713719.png" alt="Ostoskori" class="cart-icon"></div></a>
 </nav>
     
 
